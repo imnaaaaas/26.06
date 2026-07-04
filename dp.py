449aacd8c81266197fe45a18f063932ec9e6a07b
imnasss <arevadzeluka175@gmail.com>
2026-07-04 21:52:05 +0400

    added

--- dp.py
+++ dp.py
@@ -311,3 +311,22 @@ def method(nums):
 #@url-cache decorator generator
 
 #binary search / DFS / BFS for practise to get algorithms
+
+"""binary_serch
+def binary_serch(target,nums):
+    left=0
+    right=len(nums)-1
+    while left <=right:
+        mid=left+(right-left)//2 #left+space between
+        if nums[mid]==target:
+            return mid
+        elif nums[mid] <target:
+            left=mid+1
+        else:
+            right=mid-1
+    return -1
+print(binary_serch(9,[-1,0,3,5,9,12]))
+#time complexity logn we use half array
+#space complexity o(1)
+# """
+

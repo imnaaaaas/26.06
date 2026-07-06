d4cc21566220491abe41bea68bea55202946bace
imnaaaaas <arevadzeluka175@gmail.com>
2026-07-06 18:49:49 +0400

    06.07

--- dp.py
+++ dp.py
@@ -426,4 +426,24 @@ def fib(n):
     if n<=2:
         return 1
     return fib(n-1)+fib(n-2)
-print(fib(10))
+#print(fib(10))
+
+
+
+def changecase(func):
+    @functools.wraps(func)
+    def myinner(*args): 
+        for num in args:
+            if num%2==0:
+                return num 
+        return func(*args)
+    return myinner
+
+          
+@changecase
+def myfunction(*args):
+    return "no result"
+    
+
+
+print(myfunction(1,2,3,4,2))
